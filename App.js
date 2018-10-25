f8f4d7e054dd7f0dfc20c280fed9da4207ab8d52
Rajiv Shah <rajivshah1@icloud.com>
2018-10-24 23:40:53 -0400

    Add button/switch

--- App.js
+++ App.js
@@ -7,7 +7,7 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {Platform, StyleSheet, Text, View, Button, Switch} from 'react-native';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -24,6 +24,10 @@ export default class App extends Component<Props> {
         <Text style={styles.welcome}>Welcome to React Native!</Text>
         <Text style={styles.instructions}>To get started, edit App.js</Text>
         <Text style={styles.instructions}>{instructions}</Text>
+        <View>
+          <Button onPress={console.log('Pressed')} title={'Press me!'} />
+          <Switch />
+        </View>
       </View>
     );
   }
